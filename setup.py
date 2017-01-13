2a4b4d8c39ec718faa5b3631ea1b829d339413ab
jhover <jhover@cloudy.rhic.bnl.gov>
2017-01-13 17:15:15 -0500

    Update.

---
+++ setup.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+#
+# Setup prog for autopyfactory
+#
+#
+# Since script is in package "certify" we can know what to add to path
+
+import commands
+import os
+import re
+import sys
+
+from vc3 import infoservice
+release_version=infoservice.__version__
+
+from distutils.core import setup
+from distutils.command.install import install as install_org
+from distutils.command.install_data import install_data as install_data_org
+
+# Python version check. 
+major, minor, release, st, num = sys.version_info
+if major == 2:
+    if not minor >= 4:
+        print("Autopyfactory requires Python >= 2.4. Exitting.")
+        sys.exit(0)
+
+# ===========================================================
+#                D A T A     F I L E S 
+# ===========================================================
+
+libexec_files = ['libexec/%s' %file for file in os.listdir('libexec') if os.path.isfile('libexec/%s' %file)]
+
+systemd_files = [ 'etc/vc3-service-info.service'
+                 ]
+
+etc_files = ['etc/vc3-info-service.conf',
+             ]
+
+sysconfig_files = [
+             'etc/sysconfig/vc3-info-service',
+
+]
+
+logrotate_files = ['etc/logrotate/vc3-info-service',]
+
+#initd_files = ['/vc3-info-service.init',
+#               ]
+
+# NOTES: the docs are actually handled by setup.cfg. They are moved directory under /usr/share/doc/autopyfactory-<version>/
+#docs_files = ['docs/%s' %file for file in os.listdir('docs') if os.path.isfile('docs/%s' %file)]
+
+
+# -----------------------------------------------------------
+
+rpm_data_files=[('/usr/libexec', libexec_files),
+                ('/etc/vc3', etc_files),
+                ('/etc/sysconfig', sysconfig_files),
+                ('/etc/logrotate.d', logrotate_files),                                        
+#                ('/etc/init.d', initd_files),
+                ('/usr/lib/systemd/system', systemd_files),
+                #('/usr/share/doc/autopyfactory', docs_files),                                        
+               ]
+
+
+home_data_files=[('etc', libexec_files),
+                 ('etc', etc_files),
+#                 ('etc', initd_files),
+                 ('etc', sysconfig_files),
+                ]
+
+# -----------------------------------------------------------
+
+def choose_data_files():
+    rpminstall = True
+    userinstall = False
+     
+    if 'bdist_rpm' in sys.argv:
+        rpminstall = True
+
+    elif 'install' in sys.argv:
+        for a in sys.argv:
+            if a.lower().startswith('--home'):
+                rpminstall = False
+                userinstall = True
+                
+    if rpminstall:
+        return rpm_data_files
+    elif userinstall:
+        return home_data_files
+    else:
+        # Something probably went wrong, so punt
+        return rpm_data_files
+       
+# ===========================================================
+
+# setup for distutils
+setup(
+    name="vc3-info-service",
+    version=release_version,
+    description='vc3-info-service package',
+    long_description='''This package contains the VC3 Information Service''',
+    license='GPL',
+    author='John Hover',
+    author_email='jhover@bnl.gov',
+    maintainer='John Hover',
+    maintainer_email='jhover@bnl.gov',
+    url='https://github.com/vc3-project',
+    packages=['vc3',
+              ],
+    scripts = [ # Utilities and main script
+               'scripts/vc3-info-service',
+              ],
+    
+    data_files = choose_data_files()
+)
---
+++ vc3/infoservice.py
@@ -0,0 +1,279 @@
+#! /usr/bin/env python
+
+__author__ = "John Hover, Jose Caballero"
+__copyright__ = "2017 John Hover"
+__credits__ = []
+__license__ = "GPL"
+__version__ = "0.9.1"
+__maintainer__ = "John Hover"
+__email__ = "jhover@bnl.gov"
+__status__ = "Production"
+
+
+import logging
+import logging.handlers
+import threading
+
+from optparse import OptionParser
+from ConfigParser import ConfigParser
+
+
+# Add TRACE log level 
+logging.TRACE = 5
+logging.addLevelName(logging.TRACE, 'TRACE')
+def trace(self, msg, *args, **kwargs):
+    self._log(logging.TRACE, msg, args, **kwargs)
+logging.Logger.trace = trace
+
+
+class InfoService(object):
+    
+    def __init__(self, config):
+        self.log = logging.getLogger()
+        self.log.debug('InfoService class init...')
+        self.config = config
+        
+    def run(self):
+        self.log.debug('Infoservice running...')
+        
+
+
+class InfoserviceCLI(object):
+    """class to handle the command line invocation of APF. 
+       parse the input options,
+       setup everything, and run Factory class
+    """
+    def __init__(self):
+        self.options = None 
+        self.args = None
+        self.log = None
+        self.config = None
+
+        self.__presetups()
+        self.__parseopts()
+        self.__setuplogging()
+        self.__platforminfo()
+        self.__checkroot()
+        self.__createconfig()
+
+    def __presetups(self):
+        '''
+        we put here some preliminary steps that 
+        for one reason or another 
+        must be done before anything else
+        '''
+
+    
+    def __parseopts(self):
+        parser = OptionParser(usage='''%prog [OPTIONS]
+vc3-info-service is a information store for VC3
+
+This program is licenced under the GPL, as set out in LICENSE file.
+
+Author(s):
+John Hover <jhover@bnl.gov>
+''', version="%prog $Id: infoservice.py 1-13-17 23:58:06Z jhover $" )
+
+
+        parser.add_option("--trace", 
+                          dest="logLevel", 
+                          default=logging.WARNING,
+                          action="store_const", 
+                          const=logging.TRACE, 
+                          help="Set logging level to TRACE [default WARNING], super verbose")
+        parser.add_option("-d", "--debug", 
+                          dest="logLevel", 
+                          default=logging.WARNING,
+                          action="store_const", 
+                          const=logging.DEBUG, 
+                          help="Set logging level to DEBUG [default WARNING]")
+        parser.add_option("-v", "--info", 
+                          dest="logLevel", 
+                          default=logging.WARNING,
+                          action="store_const", 
+                          const=logging.INFO, 
+                          help="Set logging level to INFO [default WARNING]")
+        parser.add_option("--console", 
+                          dest="console", 
+                          default=False,
+                          action="store_true", 
+                          help="Forces debug and info messages to be sent to the console")
+        parser.add_option("--quiet", dest="logLevel", 
+                          default=logging.WARNING,
+                          action="store_const", 
+                          const=logging.WARNING, 
+                          help="Set logging level to WARNING [default]")
+        parser.add_option("--conf", dest="confFiles", 
+                          default="/etc/autopyfactory/autofactory.conf",
+                          action="store", 
+                          metavar="FILE1[,FILE2,FILE3]", 
+                          help="Load configuration from FILEs (comma separated list)")
+        parser.add_option("--log", dest="logfile", 
+                          default="syslog", 
+                          metavar="LOGFILE", 
+                          action="store", 
+                          help="Send logging output to LOGFILE or SYSLOG or stdout [default <syslog>]")
+        parser.add_option("--runas", dest="runAs", 
+                          #
+                          # By default
+                          #
+                          default=pwd.getpwuid(os.getuid())[0],
+                          action="store", 
+                          metavar="USERNAME", 
+                          help="If run as root, drop privileges to USER")
+        (self.options, self.args) = parser.parse_args()
+
+        self.options.confFiles = self.options.confFiles.split(',')
+
+    def __setuplogging(self):
+        """ 
+        Setup logging 
+        """
+        self.log = logging.getLogger()
+        if self.options.logfile == "stdout":
+            logStream = logging.StreamHandler()
+        elif self.options.logfile == 'syslog':
+            logStream = logging.handlers.SysLogHandler('/dev/log')
+        else:
+            lf = self.options.logfile
+            logdir = os.path.dirname(lf)
+            if not os.path.exists(logdir):
+                os.makedirs(logdir)
+            runuid = pwd.getpwnam(self.options.runAs).pw_uid
+            rungid = pwd.getpwnam(self.options.runAs).pw_gid                  
+            os.chown(logdir, runuid, rungid)
+            logStream = logging.FileHandler(filename=lf)    
+
+        if major == 2 and minor == 4:
+            FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d : %(message)s'
+        else:
+            FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s'
+        formatter = logging.Formatter(FORMAT)
+        formatter.converter = time.gmtime  # to convert timestamps to UTC
+        logStream.setFormatter(formatter)
+        self.log.addHandler(logStream)
+
+        # adding a new Handler for the console, 
+        # to be used only for DEBUG and INFO modes. 
+        if self.options.logLevel in [logging.DEBUG, logging.INFO]:
+            if self.options.console:
+                console = logging.StreamHandler(sys.stdout)
+                console.setFormatter(formatter)
+                console.setLevel(self.options.logLevel)
+                self.log.addHandler(console)
+        self.log.setLevel(self.options.logLevel)
+        self.log.info('Logging initialized.')
+
+
+    def _printenv(self):
+
+        envmsg = ''        
+        for k in sorted(os.environ.keys()):
+            envmsg += '\n%s=%s' %(k, os.environ[k])
+        self.log.trace('Environment : %s' %envmsg)
+
+
+    def __platforminfo(self):
+        '''
+        display basic info about the platform, for debugging purposes 
+        '''
+        self.log.info('platform: uname = %s %s %s %s %s %s' %platform.uname())
+        self.log.info('platform: platform = %s' %platform.platform())
+        self.log.info('platform: python version = %s' %platform.python_version())
+        self._printenv()
+
+    def __checkroot(self): 
+        """
+        If running as root, drop privileges to --runas' account.
+        """
+        starting_uid = os.getuid()
+        starting_gid = os.getgid()
+        starting_uid_name = pwd.getpwuid(starting_uid)[0]
+
+        hostname = socket.gethostname()
+        
+        if os.getuid() != 0:
+            self.log.info("Already running as unprivileged user %s at %s" % (starting_uid_name, hostname))
+            
+        if os.getuid() == 0:
+            try:
+                runuid = pwd.getpwnam(self.options.runAs).pw_uid
+                rungid = pwd.getpwnam(self.options.runAs).pw_gid
+                os.chown(self.options.logfile, runuid, rungid)
+                
+                os.setgid(rungid)
+                os.setuid(runuid)
+                os.seteuid(runuid)
+                os.setegid(rungid)
+
+                self._changehome()
+                self._changewd()
+
+                self.log.info("Now running as user %d:%d at %s..." % (runuid, rungid, hostname))
+                self._printenv()
+
+            
+            except KeyError, e:
+                self.log.error('No such user %s, unable run properly. Error: %s' % (self.options.runAs, e))
+                sys.exit(1)
+                
+            except OSError, e:
+                self.log.error('Could not set user or group id to %s:%s. Error: %s' % (runuid, rungid, e))
+                sys.exit(1)
+
+    def _changehome(self):
+        '''
+        Set environment HOME to user HOME.
+        '''
+        runAs_home = pwd.getpwnam(self.options.runAs).pw_dir 
+        os.environ['HOME'] = runAs_home
+        self.log.debug('Setting up environment variable HOME to %s' %runAs_home)
+
+
+    def _changewd(self):
+        '''
+        changing working directory to the HOME directory of the new user,
+        '''
+        runAs_home = pwd.getpwnam(self.options.runAs).pw_dir
+        os.chdir(runAs_home)
+        self.log.debug('Switching working directory to %s' %runAs_home)
+
+
+    def __createconfig(self):
+        """Create config, add in options...
+        """
+        if self.options.confFiles != None:
+            try:
+                self.config = ConfigParser()
+                self.config.read(self.options.confFiles)
+            except Exception, e:
+                self.log.error('Config failure')
+                sys.exit(1)
+        
+        self.config.set("global", "configfiles", self.options.confFiles)
+           
+    def run(self):
+        """
+        Create Daemon and enter main loop
+        """
+
+        from vc3.infoservice import InfoService
+
+        try:
+            self.log.info('Creating Daemon and entering main loop...')
+            infosrv = InfoService(self.config)
+            infosrv.run()
+            
+        except KeyboardInterrupt:
+            self.log.info('Caught keyboard interrupt - exitting')
+            f.join()
+            sys.exit(0)
+        except ImportError, errorMsg:
+            self.log.error('Failed to import necessary python module: %s' % errorMsg)
+            sys.exit(1)
+        except:
+            self.log.error('''Unexpected exception!''')
+            # The following line prints the exception to the logging module
+            self.log.error(traceback.format_exc(None))
+            print(traceback.format_exc(None))
+            sys.exit(1)          
