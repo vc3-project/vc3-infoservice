1955a368c3d5ed3ece3a32dbac66513874203fcd
jhover <jhover@cloudy.rhic.bnl.gov>
2017-03-03 18:04:29 -0500

    Update.

--- setup.py
+++ setup.py
@@ -17,58 +17,39 @@ from distutils.core import setup
 from distutils.command.install import install as install_org
 from distutils.command.install_data import install_data as install_data_org
 
-# Python version check. 
-major, minor, release, st, num = sys.version_info
-if major == 2:
-    if not minor >= 4:
-        print("Autopyfactory requires Python >= 2.4. Exitting.")
-        sys.exit(0)
-
-# ===========================================================
-#                D A T A     F I L E S 
-# ===========================================================
-
-libexec_files = ['libexec/%s' %file for file in os.listdir('libexec') if os.path.isfile('libexec/%s' %file)]
+#libexec_files = ['libexec/%s' %file for file in os.listdir('libexec') if os.path.isfile('libexec/%s' %file)]
 
 systemd_files = [ 'etc/vc3-service-info.service'
                  ]
 
 etc_files = ['etc/vc3-infoservice.conf',
+             'etc/vc3-infoclient.conf',
              ]
 
 sysconfig_files = [
              'etc/sysconfig/vc3-infoservice',
-
-]
+             ]
 
 logrotate_files = ['etc/logrotate/vc3-info-service',]
 
-#initd_files = ['/vc3-info-service.init',
-#               ]
+initd_files = ['etc/vc3-info-service.init',  ]
 
-# NOTES: the docs are actually handled by setup.cfg. They are moved directory under /usr/share/doc/autopyfactory-<version>/
-#docs_files = ['docs/%s' %file for file in os.listdir('docs') if os.path.isfile('docs/%s' %file)]
-
-
-# -----------------------------------------------------------
-
-rpm_data_files=[('/usr/libexec', libexec_files),
+rpm_data_files=[
+                #('/usr/libexec', libexec_files),
                 ('/etc/vc3', etc_files),
                 ('/etc/sysconfig', sysconfig_files),
                 ('/etc/logrotate.d', logrotate_files),                                        
-#                ('/etc/init.d', initd_files),
-                ('/usr/lib/systemd/system', systemd_files),
-                #('/usr/share/doc/autopyfactory', docs_files),                                        
+                ('/etc/init.d', initd_files),
+                ('/usr/lib/systemd/system', systemd_files),                                     
                ]
 
 
-home_data_files=[('etc', libexec_files),
+home_data_files=[
+                 #('etc', libexec_files),
                  ('etc', etc_files),
-#                 ('etc', initd_files),
+                 ('etc', initd_files),
                  ('etc', sysconfig_files),
                 ]
-
-# -----------------------------------------------------------
 
 def choose_data_files():
     rpminstall = True
