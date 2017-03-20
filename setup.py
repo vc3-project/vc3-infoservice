521852a3b1de5ce898dd7ca0afb2c67574aa83c7
jhover <jhover@192.168.1.102>
2017-03-20 09:08:09 -0400

    Update. Make plugin a root module.

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
     maintainer='John Hover',
     maintainer_email='jhover@bnl.gov',
     url='https://github.com/vc3-project',
+    py_modules = ['plugin'],
     packages=['vc3',
               'vc3.plugins',
               ],
