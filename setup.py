e68228d2b66dfb4d5f76016115d38abcb9baad87
Suchandra Thapa <sthapa@ci.uchicago.edu>
2017-07-25 16:05:37 -0500

    Clean up code and fix errors

--- setup.py
+++ setup.py
@@ -2,61 +2,56 @@
 #
 # Setup prog for infoservice
 
-import commands
-import os
-import re
 import sys
 
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 from vc3infoservice import infoservice
-release_version=infoservice.__version__
+release_version = infoservice.__version__
 
-from distutils.core import setup
-from distutils.command.install import install as install_org
-from distutils.command.install_data import install_data as install_data_org
-
-#libexec_files = ['libexec/%s' %file for file in os.listdir('libexec') if os.path.isfile('libexec/%s' %file)]
-
-systemd_files = [ 'etc/vc3-infoservice.service'
-                 ]
+systemd_files = ['etc/vc3-infoservice.service']
 
 etc_files = ['etc/vc3-infoservice.conf',
-             'etc/vc3-infoclient.conf',
-             ]
+             'etc/vc3-infoclient.conf',]
 
-sysconfig_files = [
-             'etc/sysconfig/vc3-infoservice',
-             ]
+sysconfig_files = ['etc/sysconfig/vc3-infoservice', ]
 
-logrotate_files = ['etc/logrotate/vc3-infoservice',]
+logrotate_files = ['etc/logrotate/vc3-infoservice', ]
 
-initd_files = ['etc/vc3-infoservice.init',  ]
+initd_files = ['etc/vc3-infoservice.init', ]
 
-rpm_data_files=[
-                #('/usr/libexec', libexec_files),
-                ('/etc/vc3', etc_files),
-                ('/etc/sysconfig', sysconfig_files),
-                ('/etc/logrotate.d', logrotate_files),                                        
-                ('/etc/init.d', initd_files),
-                ('/usr/lib/systemd/system', systemd_files),                                     
-               ]
+rpm_data_files = [('/etc/vc3', etc_files),
+                  ('/etc/sysconfig', sysconfig_files),
+                  ('/etc/logrotate.d', logrotate_files),
+                  ('/etc/init.d', initd_files),
+                  ('/usr/lib/systemd/system', systemd_files), ]
 
 
-home_data_files=[
-                 #('etc', libexec_files),
-                 ('etc', etc_files),
-                 ('etc', initd_files),
-                 ('etc', sysconfig_files),
-                ]
+home_data_files = [('etc', etc_files),
+                   ('etc', initd_files),
+                   ('etc', sysconfig_files), ]
 
-def choose_data_files():
-    if 'rpm' in sys.argv:
+
+def choose_data_file_locations():
+    rpm_install = True
+
+    if 'bdist_rpm' in sys.argv:
+        rpm_install = True
+
+    elif '--user' in sys.argv:
+        rpm_install = False
+
+    if rpm_install:
         return rpm_data_files
     else:
+        # Something probably went wrong, so punt
         return home_data_files
-       
+
 # ===========================================================
 
-# setup for distutils
 setup(
     name="vc3-info-service",
     version=release_version,
@@ -72,10 +67,8 @@ setup(
               'vc3infoservice.plugins',
               'vc3infoservice.plugins.persist'
               ],
-    scripts = [ # Utilities and main script
-               'scripts/vc3-info-service',
-               'scripts/vc3-info-client',
-              ],
-    
-    data_files = choose_data_files()
+    scripts=['scripts/vc3-info-service',
+             'scripts/vc3-info-client',
+             ],
+    data_files=choose_data_file_locations()
 )
